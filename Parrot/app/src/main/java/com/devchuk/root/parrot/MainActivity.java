9afe76e2ab64ee89dc5533c41b399fe4da16f1e6
Brian Chuk <brian_chuk@yahoo.com>
2015-02-21 16:53:48 -0500

    reverting

--- Parrot/app/src/main/java/com/devchuk/root/parrot/MainActivity.java
+++ Parrot/app/src/main/java/com/devchuk/root/parrot/MainActivity.java
@@ -1,12 +1,12 @@
 package com.devchuk.root.parrot;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
