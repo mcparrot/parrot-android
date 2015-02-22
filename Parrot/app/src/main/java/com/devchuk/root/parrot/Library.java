79e64e395b17e399f114e5f48f7f75073a123426
Brian Chuk <brian_chuk@yahoo.com>
2015-02-22 07:12:58 -0500

    names recored

--- Parrot/app/src/main/java/com/devchuk/root/parrot/Library.java
+++ Parrot/app/src/main/java/com/devchuk/root/parrot/Library.java
@@ -1,10 +1,9 @@
 package com.devchuk.root.parrot;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
@@ -16,15 +15,19 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Library extends ActionBarActivity{
+public class Library extends Activity {
     String access_token = "cf602569-5311-0187-4f59-b13a64";
 //    String username = "furnose1%40gmail.com";
+    String diffbot = "http://api.diffbot.com/v3/analyze?token=0d5c56d2a7a3a5a4ad6c644b326993c2&url=";
     String consumer_key = "38330-24d0747dc89fa03cacfbad64";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +56,6 @@ public class Library extends ActionBarActivity{
 
                 // Execute HTTP Post Request
                 HttpResponse response = httpclient.execute(httppost);
-//                respon = response.getEntity();
                 respon = EntityUtils.toString(response.getEntity());
                 return respon;
             } catch (ClientProtocolException e) {
@@ -65,7 +67,22 @@ public class Library extends ActionBarActivity{
         }
 
         protected void onPostExecute(String request_token) {
-            TextView t = (TextView) findViewById(R.id.token);
+            TextView t = (TextView) findViewById(R.id.token); //just for testing
+            ArrayList<String> values = new ArrayList<String>();
+            ArrayList<String> descriptions = new ArrayList<String>();
+            try {
+                JSONObject jObject = new JSONObject(respon);
+                JSONArray jArray = jObject.getJSONObject("list").names();
+                jObject = jObject.getJSONObject("list");
+                respon = "";
+                for (int i = 0; i < jArray.length(); i++) {
+                    values.add((jObject.getJSONObject(jArray.getString(i))).getString("given_title"));
+                    respon += values.get(i) + "\n";
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
             t.setText(respon);
         }
     }
